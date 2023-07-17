3ef69afdff4e4c366c3e264c7146d3a3d469352e
gustavoroberto1 <gustavo.souza@edu.sc.senai.br>
2023-07-17 19:06:21 -0300

    TESTE COMMIT NETBEANS

---
+++ src/semana02/desi/java/Semana02DesiJava.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana02.desi.java;
+
+/**
+ *
+ * @author Professor
+ */
+public class Semana02DesiJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
