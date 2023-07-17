7df8f1a5e8e9c315184b03c856ab3fe8eb3e1590
gustavoroberto1 <gustavo.souza@edu.sc.senai.br>
2023-07-17 19:39:02 -0300

    TESTE 2

--- src/semana02/desi/java/Semana02DesiJava.java
+++ src/semana02/desi/java/Semana02DesiJava.java
@@ -16,6 +16,10 @@ public class Semana02DesiJava {
      */
     public static void main(String[] args) {
         System.out.println("");
-    }
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");    }
     
 }
